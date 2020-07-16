a36df632e436cd79babc918d2c26558465d05cbe
maulanardy <maulanardy.dev@gmail.com>
2020-07-16 20:15:32 +0700

    update

--- resources/views/templates/navigation.blade.php
+++ resources/views/templates/navigation.blade.php
@@ -3,9 +3,9 @@
     <!-- Logo -->
     <a href="../../index2.html" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>LPJKP</b></span>
+      <span class="logo-mini"><b>BAPEL</b></span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>LPJKP</b></span>
+      <span class="logo-lg"><b>BAPEL</b>LPJKP</span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
